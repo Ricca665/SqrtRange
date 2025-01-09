02e04ac6fde76c983bd6e5a0f6b2bdab5ad9d937
riccx1000 <riccardosarti2010@gmail.com>
2025-01-09 17:41:55 +0100

    Removed useless os library and optimized code and added comments

--- range_calculate.py
+++ range_calculate.py
@@ -1,27 +1,24 @@
 import math
 import logging
-import os
 import configparser
 
 config = configparser.ConfigParser()
 
-def ClearLogFile(file):
-    with open(file, 'w'):
-        pass
-
 def Calculate(startingnumber, endingnumber, logFile, doClearLogFile):
     if doClearLogFile:
-        ClearLogFile(logFile)
+        with open(logFile, 'w'):
+            pass #We write blank to clear the log file
     else:
         pass
+    # Initialize logging
     logging.basicConfig(level=logging.DEBUG, filename=logFile, filemode="a+",
                         format="%(asctime)-15s %(levelname)-8s %(message)s")
+    
     for i in range(startingnumber, endingnumber):
         try:
             log_message = "Current number: " + str(i) + " sqrt: "+ str(math.sqrt(i))
             print(log_message)
             logging.info(log_message)
-
         except ValueError:
             log_message = "Invalid number for sqrt function: " + str(i)
             print(log_message)
