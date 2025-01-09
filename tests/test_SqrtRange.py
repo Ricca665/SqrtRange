aa946574799f73b976f842cd2e718aa3ebda45bd
riccx1000 <riccardosarti2010@gmail.com>
2025-01-09 17:58:50 +0100

    added tests

---
+++ tests/test_SqrtRange.py
@@ -0,0 +1,10 @@
+import SqrtRange
+
+testfirstnumber = -1000
+testsecondnumber = 1000
+logFile = "log.log"
+clearLog = True
+
+SqrtRange.Calculate(testfirstnumber, testsecondnumber, logFile, clearLog)
+
+exit(0)
