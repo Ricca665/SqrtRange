081cd073398595f886da3f46f5efba6e360878dc
riccx1000 <riccardosarti2010@gmail.com>
2025-01-09 18:35:06 +0100

    fixed workflow tests

--- tests/test_SqrtRange.py
+++ tests/test_SqrtRange.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import SqrtRange
 
 testfirstnumber = -10
@@ -6,4 +10,3 @@ logFile = "log.log"
 clearLog = True
 
 SqrtRange.Calculate(testfirstnumber, testsecondnumber, logFile, clearLog)
-
