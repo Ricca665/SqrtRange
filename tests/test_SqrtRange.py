ea5853c23b75db9dd4efe9ac74fe0d41ddd83b30
riccx1000 <riccardosarti2010@gmail.com>
2025-01-09 18:01:29 +0100

    modified thing

--- tests/test_SqrtRange.py
+++ tests/test_SqrtRange.py
@@ -1,10 +1,9 @@
 import SqrtRange
 
-testfirstnumber = -1000
-testsecondnumber = 1000
+testfirstnumber = -10
+testsecondnumber = 10
 logFile = "log.log"
 clearLog = True
 
 SqrtRange.Calculate(testfirstnumber, testsecondnumber, logFile, clearLog)
 
-exit(0)
